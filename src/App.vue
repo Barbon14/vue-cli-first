bcb4fae0d8ff181aacd07332ee0d7fc13790f853
BarbonFisso <barbieri.mat@hotmail.it>
2021-11-23 14:29:07 +0100

    added productCard component

--- src/App.vue
+++ src/App.vue
@@ -2,27 +2,47 @@
   <div id="app">
     <img alt="Vue logo" src="./assets/logo.png">
     <HelloWorld />
+    <section id="productList">
+      <productCard />
+      <productCard />
+      <productCard />
+      <productCard />
+      <productCard />
+    </section>
   </div>
 </template>
 
 <script>
 import HelloWorld from './components/HelloWorld.vue'
+import productCard from './components/productCard.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    HelloWorld,
+    productCard
   }
 }
 </script>
 
 <style lang="scss">
+* {
+  box-sizing: border-box;
+  padding: 0;
+  margin: 0;
+}
+
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  text-align: center;
+  text-align: center; 
   color: #2c3e50;
   margin-top: 60px;
+}
+
+#productList {
+  display: flex;
+  justify-content: space-evenly;
 }
 </style>
---
+++ src/components/productCard.vue
@@ -0,0 +1,31 @@
+<template>
+    <div class="card">
+        <img src="https://picsum.photos/200" alt="prodotto">
+        <h2>
+            Articolo
+        </h2>
+        <p>
+            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Odio nostrum minus fuga omnis blanditiis magnam esse. Voluptatem, ab. Culpa consequuntur in facilis enim earum hic labore rem officia dicta voluptatem?
+        </p>
+        <button>
+            Acquista
+        </button>
+    </div>
+</template>
+
+<style lang="scss" scoped>
+.card {
+    width: 200px;
+    border: 1px solid lightgray;
+    border-radius: 10px;
+    padding: 20px;
+    display: flex;
+    flex-direction: column;
+}
+
+.card h2,
+.card p {
+    margin: 10px 0;
+}
+
+</style>
